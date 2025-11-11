5ede64b76b8b79e3c5dfdfac0813f1b11878385a
VSidd976 <siddv06@gmail.com>
2025-11-11 19:01:08 +0200

    cli

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,16 @@
-use pest_derive::Parser;
+use pest_derive::Parser as PestParser;
+use clap_derive::Parser as ClapParser;
 
-#[derive(Parser)]
+#[derive(PestParser)]
 #[grammar = "./grammar.pest"]
 pub struct MarkdownParser;
+
+#[derive(ClapParser)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    #[arg()]
+    pub file_path: Option<String>,
+
+    #[arg(short, long)]
+    pub content: Option<String>,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use markdown_parser::*;
+use clap::Parser as ClapParser;
+use pest::Parser as PestParser;
+use std::fs;
+
+fn main() -> anyhow::Result<()>{
+    let cli = Cli::parse();
+    let input = if let Some(file_path) = cli.file_path {
+        println!("=== File parsing: {} ===\n", &file_path);
+        fs::read_to_string(file_path)?
+    }
+    else if let Some(content) = cli.content {
+        println!("=== Row parsing ===\n");
+        content
+    }
+    else {
+        return Err(anyhow::anyhow!("specify file path or row through --content"));
+    };
+    let result = MarkdownParser::parse(Rule::document, &input)?;
+    println!("{:?}", result);
+    Ok(())
 }
